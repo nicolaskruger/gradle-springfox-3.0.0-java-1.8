889e31634d026699722fb0d213941b6440bbf584
Nicolas Kruger <nicolas.nkruger@gmail.com>
2022-05-02 10:56:43 -0300

    reproducao do erro

--- src/main/java/com/example/oito/controller/RouteController.java
+++ src/main/java/com/example/oito/controller/RouteController.java
@@ -7,23 +7,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/")
 public class RouteController {
     @GetMapping
-    public ResponseEntity get(){
+    public ResponseEntity get(@RequestBody String name){
         return ResponseEntity.ok().build();
     }
     @PostMapping
-    public ResponseEntity post(){
+    public ResponseEntity post(@RequestBody String name){
         return ResponseEntity.ok().build();
     }
     @PutMapping
-    public ResponseEntity put(){
+    public ResponseEntity put(@RequestBody String name){
         return ResponseEntity.ok().build();
     }
     @PatchMapping
-    public ResponseEntity path(){
+    public ResponseEntity path(@RequestBody String name){
         return ResponseEntity.ok().build();
     }
     @DeleteMapping
-    public ResponseEntity delete(){
+    public ResponseEntity delete(@RequestBody String name){
         return ResponseEntity.ok().build();
     }
 }
